95e7c3bd3b7566b5a67557ccb0661fbeced0b161
Yuliia Kruhliak <Yuliia_Kruhliak@epam.com>
2023-03-11 20:50:30 +0200

    smoll changes 0.0

---
+++ samplers.py
@@ -0,0 +1,21 @@
+samplers = {
+  "euler_a": "Euler a",
+  "euler": "Euler",
+  "lms": "LMS",
+  "heun": "Heun",
+  "dpm_2": "DPM2",
+  "dpm_2_a": "DPM2 a",
+  "dpmpp_2s_a": "DPM++ 2S a",
+  "dpmpp_2m": "DPM++ 2M",
+  "dpmpp_sde": "DPM++ SDE",
+  "dpm_fast": "DPM fast",
+  "dpm_ad": "DPM adaptive",
+  "lms_ka": "LMS Karras",
+  "dpm_2_ka": "DPM2 Karras",
+  "dpm_2_a_ka": "DPM2 a Karras",
+  "dpmpp_2s_a_ka": "DPM++ 2S a Karras",
+  "dpmpp_2m_ka": "DPM++ 2M Karras",
+  "dpmpp_sde_ka": "DPM++ SDE Karras",
+  "ddim": "DDIM",
+  "plms": "PLMS"
+}
